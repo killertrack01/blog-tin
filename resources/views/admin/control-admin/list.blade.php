33dbce9a03b0adee17f70aa7fcf3a40e28b10648
Lumen-cpu <joongtungkpdvd@gmail.com>
2020-06-30 19:28:10 +0700

    update list admin

--- resources/views/admin/control-admin/list.blade.php
+++ resources/views/admin/control-admin/list.blade.php
@@ -2,79 +2,6 @@
 @extends('admin.layout.master')
 @section('title', 'Danh sách Admin')
 @section('content')
-<<<<<<< HEAD
-    @if( Auth::user()->role =='2' )
-    <section class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-12">
-            <div class="card">
-              <div class="card-header">
-                <h3 class="card-title">Danh sách Admin</h3>
-                    @if (session('status1'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status1') }}
-                        </div>
-                    @endif
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body">
-                <table id="listadmin" class="table table-bordered table-hover">
-                  <thead>
-                  <tr>
-                    <th>Id</th>
-                    <th>Email Admin</th>
-                    <th>Họ Tên</th>
-                    <th>Ngày Sinh</th>
-                    <th>Tel</th>
-                    <th>Thao tác</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  <tbody>
-                  @foreach ($users as $row)
-                  @if ($row->role=='0')
-                  <tr>
-                    <td>{{ $row->id }}</td>
-                    <td>{{ $row->email }}</td>
-                    <td>{{ $row->name }}</td>
-                    <td>{{ $row->dob }}</td>
-                    <td>{{ $row->tel }}</td>
-                    <td class="text-center">
-                        <form action="/delete-admin/{{ $row->id }}" method="post">
-                            {{ csrf_field() }}
-                            {{ method_field('DELETE') }}
-                                <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#exampleModal"><i class="fas fa-trash"></i></button>
-                                  <!-- Modal -->
-                                  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog">
-                                      <div class="modal-content">
-                                        <div class="modal-header">
-                                          <h5 class="modal-title" id="exampleModalLabel">Cảnh Báo Xóa Admin !!!</h5>
-                                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                          </button>
-                                        </div>
-                                        <div class="modal-body">
-                                            Bạn có chắc muốn xóa Admin ?
-                                        </div>
-                                        <div class="modal-footer">
-                                          <button type="submit" class="btn btn-danger">Xóa</button>
-                                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Hủy</button>
-                                        </div>
-                                      </div>
-                                    </div>
-                                  </div>
-                        </form>
-                    </td>
-                  </tr>
-                  @endif
-                  @endforeach
-                  </tbody>
-                </table>
-              </div>
-              <!-- /.card-body -->
-=======
 @if( Auth::user()->role =='2' )
 <section class="content">
   <div class="container-fluid">
@@ -86,7 +13,6 @@
             @if (session('status1'))
             <div class="alert alert-success" role="alert">
               {{ session('status1') }}
->>>>>>> 5ece1dfad7ea1b5264166daca0094250719f421a
             </div>
             @endif
           </div>
@@ -178,13 +104,7 @@
         BẠN KHÔNG CÓ QUYỀN XEM PHẦN NÀY!!!
       </div>
     </div>
-<<<<<<< HEAD
-    </section>
-    @endif
-@endsection
-=======
   </div>
 </section>
 @endif
 @endsection
->>>>>>> 5ece1dfad7ea1b5264166daca0094250719f421a

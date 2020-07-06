8e138e61d166cfda56412d28e98d7c53a66a044d
kinkin130501 <66099811+kinkin130501@users.noreply.github.com>
2020-07-06 11:16:49 +0700

    thêm đường link cho list post user

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -25,6 +25,7 @@
                             <a class="dropdown-item" href="{{ route('admin') }}">{{ __('Dashboard Admin') }}</a>
                             @else (Auth::user()->role =='1')
                             <a class="dropdown-item" href="{{ route('post') }}">{{ __('Đăng bài') }}</a>
+                            <a class="dropdown-item" href="{{ route('listpost') }}">{{ __('Danh sách bài cá nhân') }}</a>
                             @endif
                             <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                                 document.getElementById('logout-form').submit();">
